745c5225a97eb38f829b89df4d6add185b29d51d
Rhyen <jannatividad@gmail.com>
2022-08-31 18:14:31 +0800

    Created MyList.java

---
+++ src/prelims/MyList.java
@@ -0,0 +1,18 @@
+package prelims;
+
+import java.util.NoSuchElementException;
+
+public interface MyList <E>
+{
+
+    public int getSize();
+
+    public void insert(E data) throws ListOverflowException;
+
+    public E getElement(E data) throws NoSuchElementException;
+
+    public boolean delete(E data); // returns false if the data is not deleted in the list
+
+    public boolean search(E data);
+
+}
